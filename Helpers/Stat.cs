0faad6fe2adcb60d382a86e92384e91390703635
eliyy4hu <eliyy4hu@outlook.com>
2020-04-15 00:41:28 +0500

    group tests collect statistics but smth wrong)00

---
+++ Helpers/Stat.cs
@@ -0,0 +1,8 @@
+﻿namespace AI_Research_1.Helpers
+{
+    public class Stat
+    {
+        public StatValue FlagsCountStat = new StatValue();
+        public StatValue TicksCountStat = new StatValue();
+    }
+}
